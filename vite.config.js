59236539c05537428425b5b49101d2e72ed304e1
XINYU714 <edc2312321@gmail.com>
2023-01-15 23:02:59 +0800

    url-config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-switch-theme/",
+  base: "/react-vite-switch_theme/",
   plugins: [react()],
 });
